e73dd801ea98cb2ae379f705b47e946d8fc032b7
Verdvana <40414659+Verdvana@users.noreply.github.com>
2022-01-11 09:21:35 +0800

    Update Async_FIFO.sv

--- design/rtl/Async_FIFO.sv
+++ design/rtl/Async_FIFO.sv
@@ -5,7 +5,7 @@
 // Author:						Verdvana
 // Email:						verdvana@outlook.com
 //--------------------------------------------------------------------------------------------
-// Version 	Design		Coding		Simulata	Review		Rel data
+// Version 	Design		Coding		Simulate	Review		Rel date
 // V1.0		Verdvana	Verdvana	Verdvana		  	2019-11-05
 // V2.0		Verdvana	Verdvana	Verdvana		  	2021-08-07
 // V2.1		Verdvana	Verdvana	Verdvana		  	2021-08-07
